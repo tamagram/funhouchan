e82e502917b58ad5212559a4dec9b7bdd89f3e4a
tamagram <tamasupaka@gmail.com>
2021-04-11 06:59:31 +0900

    :sparkles: Get messages from a specific channel and send them to other channels.
    Get messages with `on_message` function, avoid duplicate events and commands with `await self.bot.process_commands` function.

--- funhouchan/controller.py
+++ funhouchan/controller.py
@@ -13,6 +13,13 @@ class Controller(object):
         @self.bot.event
         async def on_ready():
             print("funhouchan Activated!")
+        
+        @self.bot.event
+        async def on_message(message):
+            if hasattr(self.funhou, "current_channel_id"):
+                if message.channel.id == self.funhou.current_channel_id()[0]:
+                    await self.funhou.send_funhou(message.content)
+            await self.bot.process_commands(message)
 
         @self.bot.command()
         async def funhou_set(ctx, user_name, sender_channel_id, dest_channel_id):
@@ -21,5 +28,6 @@ class Controller(object):
                 del self.funhou
             self.funhou = Funhou(bot=self.bot, user_name=user_name, channel_id=channel_id)
             await self.funhou.set_channel()
-        
+
+                
         self.bot.run(self.TOKEN)
--- funhouchan/feature/funhou.py
+++ funhouchan/feature/funhou.py
@@ -4,9 +4,16 @@ class Funhou(object):
         self.bot = bot
         self._sender_channel_id = int(channel_id[0])
         self._dest_channel_id = int(channel_id[1])
+
+    def current_channel_id(self):
+        return [self._sender_channel_id, self._dest_channel_id]
     
     async def set_channel(self):
         sender_channel = self.bot.get_channel(self._sender_channel_id)
         dest_channel = self.bot.get_channel(self._dest_channel_id)
         await sender_channel.send("分報告の送り元はここに設定されたよ!")
         await dest_channel.send("分報の送り先はここに設定されたよ!")
+
+    async def send_funhou(self, message_content):
+        dest_channel = self.bot.get_channel(self._dest_channel_id)
+        await dest_channel.send(message_content)
