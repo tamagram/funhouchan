9e26d84a4b3d2c04bf8d2cb57e8d96f9cb2255de
tamagram <tamasupaka@gmail.com>
2021-04-11 05:30:00 +0900

    :recycle: Refactor code.
    Because the previous source code was so bad.

--- funhouchan/feature/funhou.py
+++ funhouchan/feature/funhou.py
@@ -1,22 +1,9 @@
 class Funhou(object):
-    def __init__(self, bot):
+    
+    def __init__(self, bot, user_name, channel_id):
         self.bot = bot
-        self._sender_channel_id = None
-        self._dest_channel_id = None
-
-    @property
-    def sender_channel_id(self):
-        return self._sender_channel_id
-    @sender_channel_id.setter
-    def sender_channel_id(self, channel_id):
-        self._sender_channel_id = int(channel_id)
-
-    @property
-    def dest_channel_id(self):
-        return self._dest_channel_id
-    @dest_channel_id.setter
-    def dest_channel_id(self, channel_id):
-        self._dest_channel_id = int(channel_id)
+        self._sender_channel_id = int(channel_id[0])
+        self._dest_channel_id = int(channel_id[1])
     
     async def set_channel(self):
         sender_channel = self.bot.get_channel(self._sender_channel_id)
--- funhouchan/main.py
+++ funhouchan/main.py
@@ -1,8 +1,6 @@
 import os
-import discord
-from discord.ext import commands
 
-from feature import funhou
+from controller import Controller 
 
 def main():
     # Checking token
@@ -13,25 +11,36 @@ def main():
         print("terminal> export DISCORD_FUNHOUCHAN=???")
         exit()
 
-    bot = commands.Bot(command_prefix='?')
+    
 
-    @bot.event
-    async def on_ready():
-        print("funhouchan Activated!")
+    controller = Controller(TOKEN)
+    controller.run()
+
+    # @bot.event
+    # async def on_ready():
+    #     print("funhouchan Activated!")
 
 
-    @bot.command()
-    async def test(ctx, arg):
-        await ctx.send(arg)
+    # @bot.command()
+    # async def test(ctx, arg):
+    #     await ctx.send(arg)
 
-    feature_funhou = funhou.Funhou(bot)
-    @bot.command()
-    async def funhou_set(ctx, user_name, sender_channel_id, dest_channel_id):
-        feature_funhou.sender_channel_id = sender_channel_id
-        feature_funhou.dest_channel_id = dest_channel_id
-        await feature_funhou.set_channel()
+    # @bot.command()
+    # async def funhou_set(ctx, user_name, sender_channel_id, dest_channel_id):
+    #     channel_id = [sender_channel_id, dest_channel_id]
+    #     main_funhou = funhou.Funhou(bot, user_name, channel_id)
+    #     await main_funhou.set_channel()
 
-    bot.run(TOKEN)
+    # @bot.command()
+    # async def tashikame(ctx):
+    #     if main_funhou == None:
+    #         await ctx.send("None!")
+    #         return
+    #     if main_funhou != None:
+    #         await ctx.send("変わってる!")
+    #         return
+        
+    # bot.run(TOKEN)
 
     
 if __name__ == "__main__":
